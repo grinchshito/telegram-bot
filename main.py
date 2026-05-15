a5edf5248f65ce9e1b787bbb2984b038273713b9
grinchshito <clases1saw@gmail.com>
2026-05-15 16:03:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,7 @@ async def buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
 
     if text == "📺 Activar TV":
-        
-usuarios_esperando_codigo.add(update.effective_user.id)
+        usuarios_esperando_codigo.add(update.effective_user.id)
         await update.message.reply_text("🔑 Envia el código de activación")
 
     elif text == "🔐 Login":
