e55db9d5bf6ad9f5a8d14e4ba069ab08dd038e95
grinchshito <clases1saw@gmail.com>
2026-05-15 12:55:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters
+)
+
+TOKEN = "8943672380:AAGyuzgm2cCRKvKmXRkYgD-3kHoQOh5L1aM"
+
+keyboard = [
+    ["📺 Activar TV", "🔐 Login"],
+    ["👤 Cuenta", "📊 Stats"],
+    ["❓ Ayuda"]
+]
+
+markup = ReplyKeyboardMarkup(
+    keyboard,
+    resize_keyboard=True
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+
+    await update.message.reply_text(
+        f"✨ Bienvenido {user.first_name}",
+        reply_markup=markup
+    )
+
+async def buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    if text == "📺 Activar TV":
+        await update.message.reply_text("TV activada")
+
+    elif text == "🔐 Login":
+        await update.message.reply_text("Login correcto")
+
+    elif text == "👤 Cuenta":
+        await update.message.reply_text("Información de cuenta")
+
+    elif text == "📊 Stats":
+        await update.message.reply_text("Tus estadísticas")
+
+    elif text == "❓ Ayuda":
+        await update.message.reply_text("Centro de ayuda")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.add_handler(
+    MessageHandler(filters.TEXT, buttons)
+)
+
+print("BOT ONLINE")
+
+app.run_polling()
